e7e69931123f30f2be3a3ed859f438fb835d1686
LitCluSby <79252525+LitCluSby@users.noreply.github.com>
2021-02-19 14:19:14 +1000

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,21 +5,16 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
+    TG_BOT_TOKEN= "1602328104:AAH6-swUwRLGv4KUrFrXsJBvHGU15pgzajc"
+    APP_ID = 3069192
+    API_HASH = "6ac35bf0f4dc633fe98e57187b6adec6"
+    OWNER_ID = 493291042
+    AUTH_CHANNEL = [-515470340]
+    DESTINATION_FOLDER = "TelegramBot" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
-    RCLONE_CONFIG = """
-[DRIVE]
-type = drive
-client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
-scope = drive
-root_folder_id =
-token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
-team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
-"""
+    RCLONE_CONFIG = """type = drive\n
+client_id = 339402946333-7kqjcr4npd0jcbp8gjecbvdnl15081ia.apps.googleusercontent.com\n
+client_secret = 146Viz2F4wiSCVkzYdrW1OIe\n
+scope = drive\n
+token = {"access_token":"ya29.A0AfH6SMCjf1lkBxSG5DK25Zm98pX_AQcDIO8hxR6ZmtyVHqGJqA9dUCmHf8LWsZlQOM-ml_gRNmDjDnCb1DRtsopb-aQaLKovqbpMxJbfhPOhqqlrX3TSQOn3-RDrf_ttixnCy5prWCoVIJjWuuYCiGbHBtkk","token_type":"Bearer","refresh_token":"1//0cW1aalFAyQAWCgYIARAAGAwSNwF-L9IrI4MapVj56WxdCyUSX3yTPMK_OtnBs1uXjVSvqrdZSXFptWwZb_wsGIpR_TeRpzBmprA","expiry":"2021-02-19T10:52:01.1612809+10:00"}"""
